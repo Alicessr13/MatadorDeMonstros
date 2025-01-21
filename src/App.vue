2d832f66775867e9b88687d2e7583ebd5ca2b523
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2025-01-20 23:20:28 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const vidaJogador = ref('100%');
-const vidaMonstro = ref('100%');
+const vidaJogador = ref(100);
+const vidaMonstro = ref(80);
+
+const jogo = ref(false);
+
+const jogoStart = () => {
+    jogo.value = !jogo.value;
+    vidaJogador.value = 100;
+    vidaMonstro.value = 100;
+}
+
+const ataque = () => {
+    vidaJogador.value -= 10;
+    vidaMonstro.value -= 10;
+}
+
 </script>
 
 
@@ -13,7 +27,8 @@ const vidaMonstro = ref('100%');
 
             <div class="w-full flex flex-col items-center">
                 <div class="h-8 w-9/12 border-2 border-gray-400">
-                    <div :class="parseInt(vidaJogador) > 20 ? 'bg-lime-600' : 'bg-red-700'" class="h-full" :style="{ width: vidaJogador }"></div>
+                    <div :class="vidaJogador > 20 ? 'bg-lime-600' : 'bg-red-700'" class="h-full"
+                        :style="{ width: vidaJogador + '%' }"></div>
 
                 </div>
                 <p class="text-3xl">{{ vidaJogador }}</p>
@@ -25,7 +40,8 @@ const vidaMonstro = ref('100%');
 
             <div class="w-full flex flex-col items-center">
                 <div class="h-8 w-9/12 border-2 border-gray-400">
-                    <div :class="parseInt(vidaMonstro) > 20 ? 'bg-lime-600' : 'bg-red-700'" class="h-full" :style="{ width: vidaMonstro}"></div>
+                    <div :class="vidaMonstro > 20 ? 'bg-lime-600' : 'bg-red-700'" class="h-full"
+                        :style="{ width: vidaMonstro + '%' }"></div>
 
                 </div>
                 <p class="text-3xl">{{ vidaMonstro }}</p>
@@ -34,11 +50,16 @@ const vidaMonstro = ref('100%');
         </div>
     </div>
 
-    <div class="m-10 p-10 border-2 flex justify-center space-x-7 text-4xl focus:outline-none focus:ring focus:ring-white">
-        <div class="bg-red-600 hover:bg-red-700   p-4 rounded-lg text-white ">ATAQUE</div>
-        <div class="bg-amber-400 hover:bg-amber-500 p-4 rounded-lg ">ATAQUE ESPECIAL</div>
-        <div class="bg-lime-400 hover:bg-lime-500 p-4 rounded-lg text-white">CURAR</div>
-        <div class="bg-gray-500 hover:bg-gray-600 p-4 rounded-lg ">DESISTIR</div>
+    <div class="m-10 p-10 border-2 flex justify-center text-4xl">
+        <div v-if="!jogo" @click="jogoStart">Iniciar novo jogo</div>
+
+        <div v-else-if="jogo" class="flex justify-center space-x-7">
+            <div class="bg-red-600 hover:bg-red-700   p-4 rounded-lg text-white " @click="ataque">ATAQUE</div>
+            <div class="bg-amber-400 hover:bg-amber-500 p-4 rounded-lg ">ATAQUE ESPECIAL</div>
+            <div class="bg-lime-400 hover:bg-lime-500 p-4 rounded-lg text-white">CURAR</div>
+            <div class="bg-gray-500 hover:bg-gray-600 p-4 rounded-lg " @click="jogoStart">DESISTIR</div>
+        </div>
+
     </div>
 </template>
 
